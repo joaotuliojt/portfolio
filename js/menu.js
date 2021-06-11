874339514029e6c4aa3909ccfc5fd2755cba27fe
iTPr0 <66981750+iTPr0@users.noreply.github.com>
2021-06-11 01:04:05 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -17,4 +17,40 @@ btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
 
+// Aqui você vê a parte "#" que está na URL à frente, mas simplesmente não mostra.
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var target = this.hash;
+        var $target = $(target);
+        var scroll;
+        if ($(window).scrollTop() == 0) {
+            scroll = ($target.offset().top) - 100
+        } else {
+            scroll = ($target.offset().top) - 90
+        }
+        $('html, body').stop().animate({
+            'scrollTop': scroll
+        }, 1000, 'swing', function() {});
+    });
+});
 
+// Você clica em Sobre mim, serviço e contato indo para a parte inferior ou superior.
+$(window).on('scroll', function() {
+    var sections = $('section')
+      , nav = $('nav')
+      , nav_height = nav.outerHeight()
+      , cur_pos = $(this).scrollTop();
+    sections.each(function() {
+        var top = $(this).offset().top - nav_height
+          , bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('li a').removeClass('active');
+            sections.removeClass('active');
+
+            $(this).addClass('active');
+            nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+        }
+    });
+});
